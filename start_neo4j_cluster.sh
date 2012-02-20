f6dcb2051f7f679eed80892eae90901463b9e859
Alistair Jones <alistair.jones@gmail.com>
2012-02-20 13:05:50 +0100

    Start script fixed by Julian's bash skills

--- start_neo4j_cluster.sh
+++ start_neo4j_cluster.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
+set -x
+NEO_DIR='neo4j-enterprise-1.6.1'
 function exec_all_servers {
   for i in 1 2 3; do
-    # command=$@
-    # echo $command
-    vagrant ssh server$i -c $@
+    vagrant ssh server$i -c "$@"
   done;
 }
 
-# exec_all_servers "neo4j-enterprise-1.6.1/bin/neo4j-coordinator start"
-# exec_all_servers "neo4j-enterprise-1.6.1/bin/neo4j start"
-
-exec_all_servers "neo4j-enterprise-1.6.1/bin/neo4j stop"
-exec_all_servers "neo4j-enterprise-1.6.1/bin/neo4j-coordinator stop"
+for command in "${NEO_DIR}/bin/neo4j-coordinator start" "${NEO_DIR}/bin/neo4j start"; do
+  exec_all_servers "$command"
+done
